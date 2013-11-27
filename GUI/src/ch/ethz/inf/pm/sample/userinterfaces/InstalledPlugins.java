ab74299f4fcb1f43f962bd82ea1dc259d5606acb
Raphael Fuchs <raphael@fuchs.tw>
2013-11-27 10:41:43 +0100

    Tidy up InstalledPlugins so that it doesn't use array indices

--- GUI/src/ch/ethz/inf/pm/sample/userinterfaces/InstalledPlugins.java
+++ GUI/src/ch/ethz/inf/pm/sample/userinterfaces/InstalledPlugins.java
@@ -24,29 +24,32 @@ public class InstalledPlugins {
     public static HeapDomain<?, ?>[] heapanalyses;
 
     static  {
-        compilers=new ch.ethz.inf.pm.sample.oorepresentation.Compiler[3];
-        compilers[0]=new ScalaCompiler();
-        compilers[1]=new TouchCompiler();
-        compilers[2]=new LoopCostCompiler();
+        compilers = new ch.ethz.inf.pm.sample.oorepresentation.Compiler[] {
+                new ScalaCompiler(),
+                new TouchCompiler(),
+                new LoopCostCompiler()
+        };
 
-        analyses=new SemanticAnalysis[9];
-        analyses[0]=new NonRelationalNumericalAnalysis();
-        analyses[1]=new AccessPermissionsAnalysis();
-        analyses[2]=new ApronAnalysis();
-        analyses[3]=new PrefixAndSuffixAnalysis();
-        analyses[4]=new BricksAnalysis();
-        analyses[5]=new SurelyAndMaybeContainedCharactersAnalysis();
-        analyses[6]=new TouchAnalysis();
-        analyses[7]=new TouchAnalysisWithApron();
-        analyses[8]=new CostAnalysis();
+        analyses = new SemanticAnalysis<?>[] {
+                new NonRelationalNumericalAnalysis(),
+                new AccessPermissionsAnalysis(),
+                new ApronAnalysis(),
+                new PrefixAndSuffixAnalysis(),
+                new BricksAnalysis(),
+                new SurelyAndMaybeContainedCharactersAnalysis(),
+                new TouchAnalysis(),
+                new TouchAnalysisWithApron(),
+                new CostAnalysis()
+        };
 
-        heapanalyses=new HeapDomain[6];
-        heapanalyses[0]=createNonRelationalMayHeapDomain(new TopHeapIdentifier(null, null));
-        heapanalyses[1]=createNonRelationalMayHeapDomain(new ClassHeapIdentifier(null, null));
-        heapanalyses[2]=createNonRelationalMayHeapDomain(new NullProgramPointHeapIdentifier(null, null, 0));
-        heapanalyses[3]=createNonRelationalMayAndMustHeapDomain(new NullProgramPointHeapIdentifier(null, null, 0));
-        heapanalyses[4]=createNonRelationalSummaryCollectionHeapDomain(new NullProgramPointHeapIdentifier(null, null, 0));
-        heapanalyses[5]=new TVSHeap();
+        heapanalyses = new HeapDomain[] {
+                createNonRelationalMayHeapDomain(new TopHeapIdentifier(null, null)),
+                createNonRelationalMayHeapDomain(new ClassHeapIdentifier(null, null)),
+                createNonRelationalMayHeapDomain(new NullProgramPointHeapIdentifier(null, null, 0)),
+                createNonRelationalMayAndMustHeapDomain(new NullProgramPointHeapIdentifier(null, null, 0)),
+                createNonRelationalSummaryCollectionHeapDomain(new NullProgramPointHeapIdentifier(null, null, 0)),
+                new TVSHeap()
+        };
 
     }
 
