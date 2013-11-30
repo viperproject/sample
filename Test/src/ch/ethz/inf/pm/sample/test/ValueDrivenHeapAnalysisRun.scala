849d2702f3cd899c4e094097ad9cc31c02674be8
milos <milos@milos>
2013-11-30 11:32:49 +0100

    Added Runner for ValueDrivenHeapAnalysis

--- Test/src/ch/ethz/inf/pm/sample/test/ValueDrivenHeapAnalysisRun.scala
+++ Test/src/ch/ethz/inf/pm/sample/test/ValueDrivenHeapAnalysisRun.scala
@@ -1,11 +1,12 @@
-package test.graph
+package ch.ethz.inf.pm.sample.test
 
 import ch.ethz.inf.pm.sample.{StdOutOutput, SystemParameters}
 import ch.ethz.inf.pm.sample.oorepresentation.scalalang.ScalaCompiler
-import ch.ethz.inf.pm.sample.abstractdomain.numericaldomain.{ApronInterface, ShowGraphProperty}
+import ch.ethz.inf.pm.sample.abstractdomain.numericaldomain.ApronInterface
 import ch.ethz.inf.pm.sample.abstractdomain.{ExpressionSet, SimpleAnalyzer}
 import ch.ethz.inf.pm.sample.property.OutputCollector
 import graph.{ValueDrivenHeapAnalysis, ValueDrivenHeapState}
+import ch.ethz.inf.pm.sample.userinterfaces.ShowGraph
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,33 +17,33 @@ import graph.{ValueDrivenHeapAnalysis, ValueDrivenHeapState}
  */
 object ValueDrivenHeapAnalysisRun {
 
-  //private val methods : List[String] = "emptyMethod" :: Nil;
-//  private val methods : List[String] = "valueVariableAssignment" :: Nil;
   private val methods : List[String] = "createNext" :: Nil;
 
   def main(args : Array[String]) : Unit = {
     //Mandatory global settings
     SystemParameters.compiler = new ScalaCompiler;
-    SystemParameters.property = new ShowGraphProperty;
+    //SystemParameters.property = new ShowGraphProperty;
+
+    SystemParameters.isValueDrivenHeapAnalysis = true
 
     SystemParameters.analysisOutput = new StdOutOutput();
     SystemParameters.progressOutput = new StdOutOutput();
 
     //Files paths
 //    val file = "/home/milos/SampleMyBranch/ValueDrivenHeapAnalysis/src/test/files/TestFiles.scala";
-    val file = "/home/milos/SampleMyBranch/ValueDrivenHeapAnalysis/src/test/files/Node.scala";
+    val file = "/home/milos/SampleBitbucket/ValueDrivenHeapAnalysis/src/test/files/Node.scala";
 
     SystemParameters.compiler.compile(file :: Nil);
 
     //EntryState
-    var entryState = new ValueDrivenHeapState[ApronInterface](new ExpressionSet(SystemParameters.getType.top)).top()
+    val entryState = new ValueDrivenHeapState[ApronInterface](new ExpressionSet(SystemParameters.getType.top)).top()
     //entryState.isBottom = true
 
     var analyzer = new ValueDrivenHeapAnalysis()
 
-    analyzer.analyze(methods, entryState, new OutputCollector);
+    val analysisResult = analyzer.analyze(methods, entryState, new OutputCollector)
 
-    System.out.println("Semantic time: " + SystemParameters.domainTimer.totalTime);
+    ShowGraph.Show[ValueDrivenHeapState[ApronInterface]](analysisResult)
 
   }
 }
