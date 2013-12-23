0329d44c1907119d97d4af41a33aecc41c53982c
Severin Heiniger <severinheiniger@gmail.com>
2013-12-23 19:15:14 +0100

    Move identifier and analysis classes to separate files. ValueDrivenHeapState.scala is big enough as it is.

---
+++ ValueDrivenHeapAnalysis/src/graph/ValueDrivenHeapAnalysis.scala
@@ -0,0 +1,41 @@
+package graph
+
+import ch.ethz.inf.pm.sample.abstractdomain.{ExpressionSet, Analysis}
+import ch.ethz.inf.pm.sample.abstractdomain.numericaldomain.ApronInterface
+import ch.ethz.inf.pm.sample.oorepresentation.NativeMethodSemantics
+import ch.ethz.inf.pm.sample.oorepresentation.scalalang.{BooleanNativeMethodSemantics, IntegerNativeMethodSemantics, ObjectNativeMethodSemantics}
+import ch.ethz.inf.pm.sample.property.Property
+import ch.ethz.inf.pm.sample.SystemParameters
+
+class ValueDrivenHeapAnalysis extends Analysis {
+
+  def getLabel(): String = "Value Driven Heap analysis"
+
+  def parameters(): List[(String, Any)] = Nil
+
+  def setParameter(label: String, value: Any) {
+    //    label match {
+    //      case "Domain" => value match {
+    //        case "Interval" => domain = new Box()
+    //        case "PPL" => domain = new Polka(false) //new PplPoly(false); FIXIT: Change back to PPL
+    //        case "Octagons" => domain = new Octagon()
+    //        case "Polka" => domain = new Polka(false)
+    //        case "Linear equalities" => domain = new PolkaEq()
+    //      }
+    //    }
+  }
+
+  def reset() {}
+
+  def getInitialState(): ValueDrivenHeapState[ApronInterface] = new ValueDrivenHeapState[ApronInterface](new ExpressionSet(SystemParameters.getType.top)).top()
+
+  def getProperties(): List[Property] = List(
+    //    new ShowGraphProperty().asInstanceOf[Property]
+    //    new SingleStatementProperty(DivisionByZero),
+    //    new SingleStatementProperty(new LowerBoundedValue("y", 0)),
+    //    new SingleStatementProperty(new BoundedValue("y", -4, 4))
+  )
+
+  def getNativeMethodsSemantics(): List[NativeMethodSemantics] = List(ObjectNativeMethodSemantics, IntegerNativeMethodSemantics, BooleanNativeMethodSemantics)
+
+}
