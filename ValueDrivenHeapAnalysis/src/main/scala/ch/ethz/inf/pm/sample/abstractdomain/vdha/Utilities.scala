d2ec4496d1a296cc85a0f6e5b17d5a9a4da653c3
Severin Heiniger <severinheiniger@gmail.com>
2014-01-11 20:06:58 +0100

    Remove unused Utilities.applyConditions.

--- ValueDrivenHeapAnalysis/src/main/scala/ch/ethz/inf/pm/sample/abstractdomain/vdha/Utilities.scala
+++ ValueDrivenHeapAnalysis/src/main/scala/ch/ethz/inf/pm/sample/abstractdomain/vdha/Utilities.scala
@@ -33,25 +33,6 @@ object Utilities {
     case _ => throw new Exception("Invalid boolean operator.")
   }
 
-  /**
-   * Pairwise GLB between states in <code>leftConds</code> and <code>rightConds</code>preserving
-   * EdgeLocalIdentifiers and AccessPathIdentifiers.
-   *
-   * @param leftConds
-   * @param rightConds
-   * @return
-   *
-   * @author Milos Novacek
-   */
-  def applyConditions[S <: SemanticDomain[S]](leftConds : Set[S], rightConds : Set[S]) : Set[S] = {
-    val resultingConds = scala.collection.mutable.Set.empty[S]
-    for (lCond <- leftConds)
-      for (rCond <- rightConds) {
-        resultingConds += glbPreserveIds(lCond, rCond)
-      }
-    resultingConds.toSet[S]
-  }
-
   def removeAccessPathIdentifiers[S <: SemanticDomain[S]](state : S) : S = {
     val idsToRemove = state.getIds().filter(id => id.isInstanceOf[AccessPathIdentifier]).toSet[Identifier]
     state.removeVariables(idsToRemove)
