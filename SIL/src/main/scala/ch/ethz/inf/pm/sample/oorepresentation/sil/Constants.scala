10b922deef52cf1a084ddded1826866dd144de8f
Severin Heiniger <severinheiniger@gmail.com>
2014-02-23 14:55:37 +0100

    Use Unicode ballot box as prefix for ghost symbols. It's a bit easier on the eyes when debugging.

--- SIL/src/main/scala/ch/ethz/inf/pm/sample/oorepresentation/sil/Constants.scala
+++ SIL/src/main/scala/ch/ethz/inf/pm/sample/oorepresentation/sil/Constants.scala
@@ -3,7 +3,7 @@ package ch.ethz.inf.pm.sample.oorepresentation.sil
 object Constants {
   // Prefix for symbols that do not exist in the original SIL program.
   // Ensures that the resulting identifier is not a valid SIL identifier.
-  val GhostSymbolPrefix = "%"
+  val GhostSymbolPrefix = "☐"
 
   // Sample variable that holds the resulting value of a SIL function.
   // Since 'result' is a keyword in SIL, there is no risk of name clashes
