ec7882fcaeee361b580cb5096b4d031e56da60f2
Severin Heiniger <severinheiniger@gmail.com>
2014-03-04 14:48:15 +0100

    Add SampleInferenceTestSuite, which currently does not infer specifications yet.

---
+++ SIL/src/test/scala/ch/ethz/inf/pm/sample/oorepresentation/sil/SampleInferenceTestSuite.scala
@@ -0,0 +1,44 @@
+package ch.ethz.inf.pm.sample.oorepresentation.sil
+
+import semper.sil.testing.SilSuite
+import semper.sil.verifier.{VerificationResult, Verifier}
+import java.nio.file.Path
+import semper.sil.frontend.Frontend
+import semper.silicon.Silicon
+import semper.sil.{ast => sil}
+
+class SiliconWithInference(private var debugInfo: Seq[(String, Any)] = Nil)
+  extends Silicon {
+
+  override def verify(program: sil.Program) = {
+    super.verify(program)
+  }
+}
+
+class SampleInferenceTestSuite extends SilSuite {
+  def testDirectories = Seq("sil/inference")
+
+  def frontend(verifier: Verifier, files: Seq[Path]): Frontend = {
+    val fe = new DummySilFrontend()
+    fe.init(verifier)
+    fe.reset(files)
+    fe
+  }
+
+  def verifiers: Seq[Verifier] = Seq(createSiliconInstance())
+
+  // Copied from silicon/src/test/scala/SiliconTests.scala
+  private def createSiliconInstance() = {
+    val silicon = new SiliconWithInference(Seq(("startedBy", "semper.silicon.SiliconTests")))
+
+    val args = optionsFromScalaTestConfigMap(configMap) ++ Seq("dummy.sil")
+
+    silicon.parseCommandLine(args)
+    silicon.config.initialize {case _ =>}
+
+    silicon
+  }
+
+  private def optionsFromScalaTestConfigMap(configMap: Map[String, Any]): Seq[String] =
+    configMap.flatMap{case (k, v) => Seq("--" + k, v.toString)}.toSeq
+}
