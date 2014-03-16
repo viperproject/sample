387b4d8ca93ae94cded9b3a46ee4c436253e26fc
Severin Heiniger <severinheiniger@gmail.com>
2014-03-17 00:15:33 +0100

    Add support for booleans to the ApronInterfaceTranslator and add a few corresponding unit tests.

--- NumericalAnalysis/src/ch/ethz/inf/pm/sample/abstractdomain/numericaldomain/ApronInterface.scala
+++ NumericalAnalysis/src/ch/ethz/inf/pm/sample/abstractdomain/numericaldomain/ApronInterface.scala
@@ -776,8 +776,8 @@ trait ApronInterface[T <: ApronInterface[T]]
     if (isTop) return "⊤ (pure)"
     val constraints = this.state.get.toLincons(domain).toList.filter(constraintContains(_, id.getName))
     if(constraints.isEmpty) return "⊤"
-    val translator = ApronInterfaceTranslator(this)
-    val exps = constraints map translator.translate map ExpPrettyPrinter
+    val translator = ApronInterfaceTranslator()(this)
+    val exps = constraints.map(translator.translate).flatten.map(ExpPrettyPrinter)
     exps.sorted.mkString("\n")
   }
 
@@ -785,7 +785,7 @@ trait ApronInterface[T <: ApronInterface[T]]
     if (isBottom) return "⊥"
     if (isTop) return "Environment: "+env+"\n"+"⊤"
 
-    val exps = ApronInterfaceTranslator.translate(this) map ExpPrettyPrinter
+    val exps = ApronInterfaceTranslator()(this).translateAll() map ExpPrettyPrinter
     if(exps.isEmpty) return "⊤"
     "Environment: "+env+"\n"+ exps.toList.sorted.mkString("\n")
   }
