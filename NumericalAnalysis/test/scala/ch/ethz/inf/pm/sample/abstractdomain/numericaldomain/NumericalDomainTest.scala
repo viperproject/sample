f248d38f54bfb7a12e786bd8cbaec3578a4f2a1a
Jérôme Dohrau <jerome.dohrau@inf.ethz.ch>
2016-04-21 17:32:25 +0200

    add tests that reveal bug in integer and double interval domain.

--- NumericalAnalysis/test/scala/ch/ethz/inf/pm/sample/abstractdomain/numericaldomain/NumericalDomainTest.scala
+++ NumericalAnalysis/test/scala/ch/ethz/inf/pm/sample/abstractdomain/numericaldomain/NumericalDomainTest.scala
@@ -17,11 +17,11 @@ trait NumericalDomainTest[T <: NumericalDomain[T]] extends SemanticDomainTest[T]
   override lazy val typ = DummyNumericalType
 
   override def values = super.values ++ Set(
-    Constant("0",typ),
-    Constant("2",typ),
-    Constant("-2",typ),
-    Constant(Int.MaxValue.toString,typ),
-    Constant(Int.MinValue.toString,typ)
+    Constant("0", typ),
+    Constant("2", typ),
+    Constant("-2", typ),
+    Constant(Int.MaxValue.toString, typ),
+    Constant(Int.MinValue.toString, typ)
   )
 
 }
@@ -35,10 +35,10 @@ trait MostPreciseAssignment[T <: SemanticDomain[T]] extends SemanticDomainTest[T
       for (a1 <- values; a2 <- values) {
         if (a1 != a2) {
           val x = factory.createVariable(v1).createVariable(v2).assign(v1, a1).assign(v2, a2)
-          assert (x.assume(v1 unequal a1).isBottom)
-          assert (x.assume(v2 unequal a2).isBottom)
-          assert (x.assume(v1 unequal v1).isBottom)
-          assert (x.assume(v2 unequal v2).isBottom)
+          assert(x.assume(v1 unequal a1).isBottom)
+          assert(x.assume(v2 unequal a2).isBottom)
+          assert(x.assume(v1 unequal v1).isBottom)
+          assert(x.assume(v2 unequal v2).isBottom)
         }
       }
   }
@@ -52,15 +52,18 @@ trait NonRelationalNumericalDomainTest[T <: NonRelationalNumericalDomain[T]] ext
 
 
   test("2 + 2 = 4") {
-    assert { !((two sum two) glb four).isBottom }
+    assert {
+      !((two sum two) glb four).isBottom
+    }
   }
 
   test("2 * 2 = 4") {
-    assert { !((two multiply two) glb four).isBottom }
+    assert {
+      !((two multiply two) glb four).isBottom
+    }
   }
 
 }
-
 
 
 class IntegerIntervalTest extends NonRelationalNumericalDomainTest[IntegerInterval] {
@@ -68,11 +71,21 @@ class IntegerIntervalTest extends NonRelationalNumericalDomainTest[IntegerInterv
   override lazy val factory = IntegerInterval.Bottom
 
   override lazy val instances: Set[IntegerInterval] = super.instances ++ Set(
-    IntegerInterval.Inner(-1,Int.MaxValue),
-    IntegerInterval.Inner(Int.MinValue,1),
-    IntegerInterval.Inner(0,0)
+    IntegerInterval.Inner(-1, Int.MaxValue),
+    IntegerInterval.Inner(Int.MinValue, 1),
+    IntegerInterval.Inner(0, 0)
   )
 
+  test("[10, 20] / [-2, 2] = [-20, 20]") {
+    val a = IntegerInterval.Inner(10, 20)
+    val b = IntegerInterval.Inner(-2, 2)
+    val low = instances.head.evalConstant(-20)
+    val high = instances.head.evalConstant(20)
+    assert {
+      !((a divide b) glb low).isBottom
+      !((a divide b) glb high).isBottom
+    }
+  }
 }
 
 
@@ -81,11 +94,18 @@ class DoubleIntervalTest extends NonRelationalNumericalDomainTest[DoubleInterval
   override def factory: DoubleInterval = DoubleInterval.Bottom
 
   override lazy val instances: Set[DoubleInterval] = super.instances ++ Set(
-    DoubleInterval.Inner(-1,Double.PositiveInfinity),
-    DoubleInterval.Inner(Double.NegativeInfinity,1),
-    DoubleInterval.Inner(0,0)
+    DoubleInterval.Inner(-1, Double.PositiveInfinity),
+    DoubleInterval.Inner(Double.NegativeInfinity, 1),
+    DoubleInterval.Inner(0, 0)
   )
 
+  test("[10, 20] / [-2, 2] = Top") {
+    val a = DoubleInterval.Inner(10, 20)
+    val b = DoubleInterval.Inner(-2, 2)
+    assert {
+      (a divide b).isTop
+    }
+  }
 }
 
 
@@ -114,7 +134,7 @@ trait BoxedNonRelationalNumericalDomainTest[T <: NonRelationalNumericalDomain[T]
 
   override def factory = BoxedNonRelationalNumericalDomain[T](domain)
 
-  def domain:T
+  def domain: T
 
 }
 
