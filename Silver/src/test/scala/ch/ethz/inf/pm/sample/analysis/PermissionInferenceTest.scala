bd614861708a49761699d6125d59ebab20208725
Caterina Urban <caterina.urban@gmail.com>
2016-04-26 12:56:22 +0200

    attempt

--- Silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/PermissionInferenceTest.scala
+++ Silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/PermissionInferenceTest.scala
@@ -59,6 +59,34 @@ class SiliconWithPermissionIntervalsInference(private var debugInfo: Seq[(String
 
 }
 
+class SiliconWithPermissionPolyhedraInferenceTestSuite extends SilSuite {
+  override def testDirectories = Seq("silver/permissions/basic")
+
+  override def frontend(verifier: Verifier, files: Seq[Path]): Frontend = {
+    val fe = new DummySilFrontend()
+    fe.init(verifier)
+    fe.reset(files)
+    fe
+  }
+
+  override def verifiers: Seq[Verifier] = Seq(createSiliconInstance())
+
+  // copied from silicon/src/test/scala/SiliconTests.scala
+  private def createSiliconInstance(): Silicon = {
+    val silicon = new SiliconWithPermissionPolyhedraInference(Seq(("startedBy", "viper.silicon.SiliconTests")))
+    val args = optionsFromScalaTestConfigMap(configMap) ++ Seq("dummy.sil")
+
+    silicon.parseCommandLine(args)
+    silicon.config.initialize { case _ => silicon.config.initialized = true }
+
+    silicon
+  }
+
+  private def optionsFromScalaTestConfigMap(configMap: Map[String, Any]): Seq[String] =
+    configMap.flatMap{case (k, v) => Seq("--" + k, v.toString)}.toSeq
+
+}
+
 class SiliconWithPermissionPolyhedraInference(private var debugInfo: Seq[(String, Any)] = Nil) extends Silicon {
 
   override val name: String = "sample"
@@ -69,7 +97,7 @@ class SiliconWithPermissionPolyhedraInference(private var debugInfo: Seq[(String
     // extend the program with the inferred permissions
     val extendedProgram = runner.extendProgram(DefaultSilConverter.prog,results)
     // use silicon to verify the extended program
-    super.verify(extendedProgram)
+    start(); super.verify(extendedProgram)
   }
 
 }
