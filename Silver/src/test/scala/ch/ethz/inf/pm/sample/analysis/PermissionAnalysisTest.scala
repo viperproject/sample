e173259fe5f85c6fa6035bf4ab69a26b30adc5e9
Jérôme Dohrau <jerome.dohrau@inf.ethz.ch>
2016-09-22 14:10:36 +0200

    fix issue #78

--- Silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/PermissionAnalysisTest.scala
+++ Silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/PermissionAnalysisTest.scala
@@ -13,14 +13,15 @@ import ch.ethz.inf.pm.sample.oorepresentation.silver.DefaultSilverConverter
 import ch.ethz.inf.pm.sample.permissionanalysis._
 import ch.ethz.inf.pm.sample.test.LatticeTest
 import viper.silicon.Silicon
-import viper.silver.ast.Program
+import viper.silver.ast._
 import viper.silver.frontend.Frontend
 import viper.silver.testing.SilSuite
-import viper.silver.verifier.{Success, VerificationResult, Verifier}
+import viper.silver.verifier._
 
 /** Testing of Permission Analysis.
   *
   * @author Caterina Urban
+  * @author Jerome Dohrau
   */
 class PermissionAnalysisTest extends SilSuite {
   override def testDirectories = Seq("silver/inference")
@@ -49,11 +50,54 @@ class SiliconWithPermissionAnalysis(private var debugInfo: Seq[(String, Any)] = 
     val runner = PermissionAnalysis
     val results = runner.run(program) // run the permission inference
     // extend the program with the inferred permissions
-    val extendedProgram = runner.extendProgram(DefaultSilverConverter.prog,results)
-    try { // use silicon to verify the extended program
-      start(); super.verify(extendedProgram)
+    val extendedProgram = runner.extendProgram(DefaultSilverConverter.prog, results)
+
+    try {
+      // use silicon to verify the extended program
+      start()
+      val result = super.verify(extendedProgram)
+
+      // use silicon to verify that the inferred preconditions are feasible
+      val errors = feasibilityPrograms(extendedProgram).foldLeft(List.empty[AbstractError]) {
+        case (err, prog) => super.verify(prog) match {
+          case Success =>
+            // the precondition is infeasible since the "assert(false)" succeeded
+            val method = program.methods.head
+            InfeasiblePrecondition(method.name, method.pos) :: err
+          case Failure(_) => err
+        }
+      }
+
+      if (errors.nonEmpty) result match {
+        case Failure(verificationErrors) => Failure(verificationErrors ++ errors)
+        case Success => Failure(errors)
+      } else result
     } catch {
       case _: Throwable => Success // something went wrong with the verifier (not our fault)
+    }
+  }
+
+  /** Creates a program for every method in the given program that contains only
+    * that method with a feasibility check. The feasibility check is an
+    * "assert(false)" at the beginning of the body. If the precondition is
+    * feasible then the assertion should fail. If the assertion succeeds then
+    * the precondition is infeasible.
+    *
+    * @param program The program.
+    * @return The sequence of programs.
+    */
+  private def feasibilityPrograms(program: Program): Seq[Program] = {
+    program.methods.map { method =>
+      // the feasibility check
+      val check = Assert(FalseLit()())()
+      // add the feasibility check to the body of the current method
+      val body = method.body match {
+        case sequence: Seqn => sequence.copy(check +: sequence.ss)(sequence.pos, sequence.info)
+        case statement => Seqn(check :: statement :: Nil)(statement.pos, statement.info)
+      }
+      // create program that only contains the current method with the check
+      val methods = Seq(method.copy(_body = body)(method.pos, method.info))
+      program.copy(methods = methods)(program.pos, program.info)
     }
   }
 }
@@ -65,4 +109,11 @@ class SiliconWithPermissionAnalysis(private var debugInfo: Seq[(String, Any)] = 
 class PermissionAnalysisLatticeTest extends LatticeTest[PermissionAnalysisState.Default] {
   SystemParameters.typ = DummyRefType
   override def factory: PermissionAnalysisState.Default = PermissionAnalysisEntryState.topState
+}
+
+case class InfeasiblePrecondition(method: String, pos: Position) extends AbstractError {
+
+  override def fullId: String = "precondition.infeasible"
+
+  override def readableMessage: String = s"Inferred precondition of method $method is unfeasible"
 }
