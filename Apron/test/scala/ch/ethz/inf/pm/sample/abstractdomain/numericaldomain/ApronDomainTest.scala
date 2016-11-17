baceb128a217f19cb9df3edc6d36ef960ec60564
Caterina Urban <caterina.urban@gmail.com>
2016-11-17 22:23:09 +0100

    sorry Lucas… now it is fixed (I think)

--- Apron/test/scala/ch/ethz/inf/pm/sample/abstractdomain/numericaldomain/ApronDomainTest.scala
+++ Apron/test/scala/ch/ethz/inf/pm/sample/abstractdomain/numericaldomain/ApronDomainTest.scala
@@ -1,16 +1,42 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 package ch.ethz.inf.pm.sample.abstractdomain.numericaldomain
 
 import ch.ethz.inf.pm.sample.SystemParameters
-import ch.ethz.inf.pm.sample.abstractdomain.{SemanticDomain, Constant}
+import ch.ethz.inf.pm.sample.abstractdomain.{Constant, SemanticDomain}
 import ch.ethz.inf.pm.sample.oorepresentation.DummyNumericalType
-import ch.ethz.inf.pm.sample.test.{SemanticDomainTest, LatticeTest}
+import ch.ethz.inf.pm.sample.test.SemanticDomainTest
 
+trait NumericalDomainTest[T <: NumericalDomain[T]] extends SemanticDomainTest[T] {
+
+  override lazy val typ = DummyNumericalType
+
+  override def values = super.values ++ Set(
+    Constant("0", typ),
+    Constant("2", typ),
+    Constant("-2", typ),
+    Constant(Int.MaxValue.toString, typ),
+    Constant(Int.MinValue.toString, typ)
+  )
+
+}
+
+// Not necessarily satisfied by all domains, e.g. Sign
+trait MostPreciseAssignment[T <: SemanticDomain[T]] extends SemanticDomainTest[T] {
+
+  val ignoreMostPreciseAssignment = false
+  test("Most precise assignment of variables") {
+    if (!ignoreMostPreciseAssignment)
+      for (a1 <- values; a2 <- values) {
+        if (a1 != a2) {
+          val x = factory.createVariable(v1).createVariable(v2).assign(v1, a1).assign(v2, a2)
+          assert(x.assume(v1 unequal a1).isBottom)
+          assert(x.assume(v2 unequal a2).isBottom)
+          assert(x.assume(v1 unequal v1).isBottom)
+          assert(x.assume(v2 unequal v2).isBottom)
+        }
+      }
+  }
+
+}
 
 trait ApronTest[T <: Apron[T]]
   extends NumericalDomainTest[T]
