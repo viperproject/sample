2e316ba34b231e438ad5dda0e79dfefa5579a1aa
Severin Münger <muengers@student.ethz.ch>
2016-12-24 00:40:51 +0100

    introduce function call expressions

--- sample/src/ch/ethz/inf/pm/sample/abstractdomain/AbstractState.scala
+++ sample/src/ch/ethz/inf/pm/sample/abstractdomain/AbstractState.scala
@@ -20,7 +20,7 @@ object ExpressionSetFactory {
     else expand(parameterExpressions.init).flatMap(seq => parameterExpressions.last.s.toSetOrFail.map(expr => seq :+ expr))
   }
 
-  def createFunctionCallExpression(typ: Type, functionName: String, parameterExpressions: Seq[ExpressionSet], pp: ProgramPoint): ExpressionSet = {
+  def createFunctionCallExpression(functionName: String, parameterExpressions: Seq[ExpressionSet], typ: Type, pp: ProgramPoint): ExpressionSet = {
     ExpressionSet((for (params <- expand(parameterExpressions)) yield {
       FunctionCallExpression(functionName, params, typ, pp)
     }).toSeq)
