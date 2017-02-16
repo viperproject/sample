ba758c9357a1c026c25881cf28b533988e15fae4
Jérôme Dohrau <jerome.dohrau@inf.ethz.ch>
2017-02-16 14:40:55 +0100

    Remove unused imports.

--- Silver/src/main/scala/ch/ethz/inf/pm/sample/abstractdomain/NonRelationalNumericalAnalysis.scala
+++ Silver/src/main/scala/ch/ethz/inf/pm/sample/abstractdomain/NonRelationalNumericalAnalysis.scala
@@ -8,7 +8,7 @@ package ch.ethz.inf.pm.sample.abstractdomain
 
 import ch.ethz.inf.pm.sample.abstractdomain.numericaldomain.{BoxedNonRelationalNumericalDomain, IntegerInterval, NonRelationalNumericalDomain}
 import ch.ethz.inf.pm.sample.execution._
-import ch.ethz.inf.pm.sample.oorepresentation.silver.{SilverAnalysisRunner, SilverMethodDeclaration, SilverProgramDeclaration}
+import ch.ethz.inf.pm.sample.oorepresentation.silver.SilverAnalysisRunner
 import ch.ethz.inf.pm.sample.oorepresentation.{DummyProgramPoint, ProgramPoint, Type}
 import ch.ethz.inf.pm.sample.permissionanalysis.{EnterLoopCommand, LeaveLoopCommand}
 import com.typesafe.scalalogging.LazyLogging
