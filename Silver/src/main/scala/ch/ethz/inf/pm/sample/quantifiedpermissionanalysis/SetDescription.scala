109fd25d3c5c0ce9ff19de2160f93ed58134a04f
Severin Münger <muengers@student.ethz.ch>
2017-03-19 11:05:07 +0100

    print default triggers

--- Silver/src/main/scala/ch/ethz/inf/pm/sample/quantifiedpermissionanalysis/SetDescription.scala
+++ Silver/src/main/scala/ch/ethz/inf/pm/sample/quantifiedpermissionanalysis/SetDescription.scala
@@ -282,9 +282,12 @@ object ReferenceSetDescription {
             if (QuantifiedPermissionsParameters.addReceiverNullCheckInSetDefinition) conjuncts :+= sil.And(sil.NeCmp(quantifiedVariableForFieldsVar, sil.NullLit()())(), contains)()
             if (!useFieldAccessFunctionsInSetDefinitions && usePermissionCheckInFieldAdd) conjuncts :+= sil.PermGtCmp(sil.CurrentPerm(fieldAccess)(), sil.NoPerm()())()
             if (isNullProhibited) conjuncts :+= sil.NeCmp(fieldAccessExpr, sil.NullLit()())()
+            val ghostForall = sil.Forall(Seq(quantifiedVariableForFields), Seq(), sil.Implies(conjuncts.reduce(sil.And(_, _)()), sil.AnySetContains(fieldAccessExpr, set)())())()
+
             val triggers =
               if (QuantifiedPermissionsParameters.useCustomTriggerGeneration) Seq(sil.Trigger(Seq(contains, fieldAccessExpr))())
               else Seq()
+            println(s"Custom triggers: $triggers\nAuto triggers would be: ${ghostForall.autoTrigger.triggers}")
             val forall = sil.Forall(Seq(quantifiedVariableForFields), triggers, sil.Implies(conjuncts.reduce(sil.And(_, _)()), sil.AnySetContains(fieldAccessExpr, set)())())()
             fields :+= forall
           case Function(functionName, _, _, argKeys) =>
