a706de7929f9f56c41c47da555f0233b92395c23
Flurin Rindisbacher <rflurin@student.ethz.ch>
2017-03-24 16:13:40 +0100

    Add support for Compilable.Code to SilverCompiler

--- sample-silver/src/main/scala/ch/ethz/inf/pm/sample/oorepresentation/silver/SilverCompiler.scala
+++ sample-silver/src/main/scala/ch/ethz/inf/pm/sample/oorepresentation/silver/SilverCompiler.scala
@@ -30,30 +30,34 @@ class SilverCompiler {
     * TODO: Does not support directories (multiple files) as input at the moment.
     * TODO: Contains absolutely no error handling.
     */
-  def compile(compilable: Compilable): SilverProgramDeclaration = compilable match {
-    case Compilable.Path(file) =>
-      val input = Source.fromInputStream(Files.newInputStream(file)).mkString
-      val parseResult = FastParser.parse(input, file)
-      val parsed = parseResult match {
-        case fastparse.core.Parsed.Success(e: PProgram, _) =>
-          e.initProperties()
-          e
-        case fastparse.core.Parsed.Failure(msg, next, extra) =>
-          throw new ParseException(s"$msg in $file at ${extra.line}:${extra.col}", 0)
-        case ParseError(msg, pos) =>
-          val (line, col) = pos match {
-            case SourcePosition(_, line, col) => (line, col)
-            case FilePosition(_, line, col) => (line, col)
-            case _ => ??? // should never happen
-          }
-          throw new ParseException(s"$msg in $file at $line:$col", 0)
-      }
+  def compile(compilable: Compilable): SilverProgramDeclaration = {
+    val (parseResult,label) = compilable match {
+      case Compilable.Path(file) =>
+        val input = Source.fromInputStream(Files.newInputStream(file)).mkString
+        (FastParser.parse(input, file), file)
+      case Compilable.Code(label, input) =>
+        (FastParser.parse(input, null), label)
+      case _ => throw new UnsupportedOperationException("Compilable " + compilable + " not supported by this compiler")
+    }
+    val parsed = parseResult match {
+      case fastparse.core.Parsed.Success(e: PProgram, _) =>
+        e.initProperties()
+        e
+      case fastparse.core.Parsed.Failure(msg, next, extra) =>
+        throw new ParseException(s"$msg in $label at ${extra.line}:${extra.col}", 0)
+      case ParseError(msg, pos) =>
+        val (line, col) = pos match {
+          case SourcePosition(_, line, col) => (line, col)
+          case FilePosition(_, line, col) => (line, col)
+          case _ => ??? // should never happen
+        }
+        throw new ParseException(s"$msg in $label at $line:$col", 0)
+    }
 
-      Resolver(parsed).run
+    Resolver(parsed).run
 
-      val program = Translator(parsed).translate.get
-      compileProgram(program)
-    case _ => throw new UnsupportedOperationException("Compilable " + compilable + " not supported by this compiler")
+    val program = Translator(parsed).translate.get
+    compileProgram(program)
   }
 
   def compileProgram(program: sil.Program): SilverProgramDeclaration = {
