fed1ef4bf4eee54840928b4f486a246bcddff0d5
Flurin Rindisbacher <rflurin@student.ethz.ch>
2017-03-27 10:53:36 +0200

    Test for trivial analysis with one variable

--- sample-silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/InterproceduralAnalysisTest.scala
+++ sample-silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/InterproceduralAnalysisTest.scala
@@ -1,7 +1,9 @@
 package ch.ethz.inf.pm.sample.analysis
 
-import ch.ethz.inf.pm.sample.abstractdomain.IntegerIntervalAnalysis
-import ch.ethz.inf.pm.sample.oorepresentation.Compilable
+import ch.ethz.inf.pm.sample.abstractdomain.{IntegerIntervalAnalysis, IntegerIntervalAnalysisState, VariableIdentifier}
+import ch.ethz.inf.pm.sample.execution.CfgResult
+import ch.ethz.inf.pm.sample.oorepresentation.{Compilable, DummyIntegerType}
+import ch.ethz.inf.pm.sample.reporting.{Reporter, SampleMessage}
 import ch.ethz.inf.pm.sample.test.SampleTest
 import org.scalatest.FunSuite
 
@@ -11,7 +13,7 @@ import org.scalatest.FunSuite
 class InterproceduralAnalysisTest extends FunSuite with SampleTest {
 
   test("trivial") {
-    run(
+    val (assertions, result) = run(
       """
          method main() {
              var x: Int
@@ -21,25 +23,26 @@ class InterproceduralAnalysisTest extends FunSuite with SampleTest {
          }
          method foo() returns (k: Int) {
              k := 1
+             assert k == 1
          }
       """.stripMargin
     )
-    //TODO check for failed asserts
+    val x = result("main").exitState().getVariableValue(VariableIdentifier("x")(DummyIntegerType))
+    assert(x.domain.dom.isTop, "x is set to top at the end of main()")
+    assert(assertions.size == 1, "only one assertion fails")
   }
 
-  def run(s: String) = {
+  /**
+    *
+    * @param s silver program as string
+    * @return Tuple with set of assertion errors and the result of the analysis in the integer interval domain
+    */
+  def run(s: String): (Set[SampleMessage], Map[String, CfgResult[IntegerIntervalAnalysisState]]) = {
     val results = IntegerIntervalAnalysis.run(
       Compilable.Code("(no name)", s)
     )
-    //TODO return some kind of error measurement. Asserts?
-    //return
-
-//    val cfgResults = results.map { case (id, cfgResult) => id.name -> cfgResult }
-//    println("\n*******************\n* Analysis Result *\n*******************\n")
-//    for ((method, cfgResult) <- cfgResults) {
-//      println("******************* " + method + "\n")
-//      cfgResult.print()
-//    }
+    val cfgResults = results.map { case (id, cfgResult) => id.name -> cfgResult }
+    (Reporter.assertionViolations, cfgResults)
   }
 
 }
