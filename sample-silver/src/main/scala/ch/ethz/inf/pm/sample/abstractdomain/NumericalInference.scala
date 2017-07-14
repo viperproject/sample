e80f7555465215df37e0ecd51e6478c8eb040428
Flurin Rindisbacher <rflurin@student.ethz.ch>
2017-07-14 20:15:27 +0200

    Infer invariants as "method-precondition => invariant" for each available CfgResult

--- sample-silver/src/main/scala/ch/ethz/inf/pm/sample/abstractdomain/NumericalInference.scala
+++ sample-silver/src/main/scala/ch/ethz/inf/pm/sample/abstractdomain/NumericalInference.scala
@@ -9,7 +9,7 @@ package ch.ethz.inf.pm.sample.abstractdomain
 import ch.ethz.inf.pm.sample.abstractdomain.numericaldomain.{IntegerOctagons, NumericalDomain}
 import ch.ethz.inf.pm.sample.execution.{BlockPosition, CfgResult, InterproceduralSilverForwardAnalysis, SilverAnalysis}
 import ch.ethz.inf.pm.sample.oorepresentation.silver.{DefaultSampleConverter, InterproceduralSilverInferenceRunner, SilverInferenceRunner}
-import viper.silver.ast.{And, Exp, Field, Or}
+import viper.silver.ast._
 
 /**
   * An inference based on a numerical analysis.
@@ -44,27 +44,51 @@ trait NumericalInferenceRunner[S <: NumericalAnalysisState[S, D], D <: Numerical
 trait InterproceduralNumericalInferenceRunner[S <: NumericalAnalysisState[S, D], D <: NumericalDomain[D]]
   extends NumericalInferenceRunner[S, D] with InterproceduralSilverInferenceRunner[Set[Expression], S] {
 
+  private def asConjunction(specifications: Set[Expression]): Option[Exp] = {
+    if (specifications.nonEmpty)
+      Some(specifications.map(DefaultSampleConverter.convert).reduce((left, right) => And(left, right)()))
+    else
+      None
+  }
+
   //
   // For the interprocedural case we take all possible method-call entry states and extend the program with a disjunction
   //
   override def preconditions(existing: Seq[Exp], position: BlockPosition, result: CfgResult[S]): Seq[Exp] = {
     // get a set of inferred preconditions for each method-call (call-string)
-    val inferred: Seq[Set[Expression]] = resultsToWorkWith.map(_.preStateAt(position).specifications).filter(_.nonEmpty)
-    // represent each set of preconditions as one And()
-    val conjuctionPerCall: Seq[Exp] = inferred.map(_.map(DefaultSampleConverter.convert).reduce((left, right) => And(left, right)()))
-    // Or() alls the possible preconditions
-    if (conjuctionPerCall.nonEmpty) {
-      val additionalPreconditions: Exp = conjuctionPerCall.reduce((left, right) => Or(left, right)())
-      existing :+ additionalPreconditions
+    val inferred: Seq[Set[Expression]] = resultsToWorkWith.map(_.preStateAt(position).specifications)
+    // represent each set of preconditions as a conjunction
+    val conjuctionsPerCall: Seq[Exp] = inferred.flatMap(asConjunction)
+    // Or() all the possible preconditions
+    if (conjuctionsPerCall.nonEmpty) {
+      val inferredPreconditions: Exp = conjuctionsPerCall.reduce((left, right) => Or(left, right)())
+      existing :+ inferredPreconditions
     } else {
       existing
     }
   }
 
+  //
+  // we add invariants in the form of
+  //    method-precondition => invariant
+  // for all the call-strings we saw during analysis
+  //
   override def invariants(existing: Seq[Exp], position: BlockPosition, result: CfgResult[S]): Seq[Exp] = {
-    val inferred = resultsToWorkWith.map(_.preStateAt(position)).reduce(_ lub _).specifications
-    val converted = inferred.map(DefaultSampleConverter.convert)
-    existing ++ converted
+    val inferredInvariants: Seq[Exp] = {
+      for (result <- resultsToWorkWith) yield {
+        val precondition = asConjunction(result.entryState().specifications)
+        val inferred = result.preStateAt(position).specifications
+        val converted = inferred.map(DefaultSampleConverter.convert)
+        // add (precondition => invariant) for every encountered call-string
+        precondition match {
+          case Some(p) =>
+            converted.map(Implies(p, _)())
+          case None =>
+            converted
+        }
+      }
+    }.flatten
+    existing ++ inferredInvariants
   }
 }
 
