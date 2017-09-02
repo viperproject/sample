d01949fdb7e433edd99504204ee6fadf9bd5452b
Jérôme Dohrau <jerome.dohrau@inf.ethz.ch>
2017-09-02 15:10:35 +0200

    Add checks to compare against expected specifications.

--- sample-silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/QuantifiedPermissionTest.scala
+++ sample-silver/src/test/scala/ch/ethz/inf/pm/sample/analysis/QuantifiedPermissionTest.scala
@@ -10,6 +10,7 @@ import ch.ethz.inf.pm.sample.abstractdomain.State
 import ch.ethz.inf.pm.sample.inference.SilverExtender
 import ch.ethz.inf.pm.sample.quantifiedpermissionanalysis.{QuantifiedPermissionsAnalysisRunner, QuantifiedPermissionsState}
 import ch.ethz.inf.pm.sample.util.Verifiers
+import viper.silver.{ast => sil}
 import viper.silver.testing._
 import viper.silver.verifier._
 
@@ -37,15 +38,25 @@ abstract class InferenceTest[S <: State[S]]
       val program = inference.compile(path)
 
       val methods = program.methods.filterNot(_.name.endsWith("_expected"))
+      val specifications = program.methods
+        .filter(_.name.endsWith("_expected"))
+        .foldLeft(Map.empty[String, sil.Method]) { (result, method) => result + (method.name.dropRight(9) -> method) }
 
       val filtered = program.copy(methods = methods)(program.pos, program.info, program.errT)
       val extended = inference.extend(filtered)
 
-      println(s"ORIGINAL:\n$program")
-      println(s"EXTENDED:\n$extended")
+      val checks = extended.methods.flatMap { inferred =>
+        specifications.get(inferred.name) match {
+          case Some(expected) => createChecks(inferred, expected)
+          case None => Seq.empty
+        }
+      }
+
+      val extendedWithChecks = extended.copy(methods = extended.methods ++ checks)(pos = extended.pos, info = extended.info, errT = extended.errT)
+      println(extendedWithChecks)
 
       val verifier = carbon
-      val result = verifier.verify(extended)
+      val result = verifier.verify(extendedWithChecks)
 
       result match {
         case Success => Seq.empty
@@ -54,6 +65,19 @@ abstract class InferenceTest[S <: State[S]]
           case error => SilOutput(error)
         }
       }
+    }
+
+    def createChecks(inferred: sil.Method, expected: sil.Method): Seq[sil.Method] = {
+      val name = inferred.name
+      val parameters = inferred.formalArgs
+      val body = sil.Seqn(Seq.empty, Seq.empty)()
+
+      val check0 = sil.Method(name + "_pre_inferred_implies_expected", parameters, Seq.empty, inferred.pres, expected.pres, body)()
+      val check1 = sil.Method(name + "_pre_expected_implies_inferred", parameters, Seq.empty, expected.pres, inferred.pres, body)()
+      val check2 = sil.Method(name + "_post_inferred_implies_expected", parameters, Seq.empty, inferred.posts, expected.posts, body)()
+      val check3 = sil.Method(name + "_post_expected_implies_inferred", parameters, Seq.empty, expected.posts, inferred.posts, body)()
+
+      Seq(check0, check1, check2, check3)
     }
 
     /**
