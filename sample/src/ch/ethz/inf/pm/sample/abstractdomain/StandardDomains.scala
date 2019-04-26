187ac12bd90aeda0192d97a9e6e31ea99649ae89
Jérôme Dohrau <jerome.dohrau@inf.ethz.ch>
2019-04-26 12:55:15 +0200

    Update license.

--- sample/src/ch/ethz/inf/pm/sample/abstractdomain/StandardDomains.scala
+++ sample/src/ch/ethz/inf/pm/sample/abstractdomain/StandardDomains.scala
@@ -2,6 +2,8 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2011-2019 ETH Zurich.
  */
 
 //This file contains several implementations of some standard domains. These standard domains are quite useful to develop
